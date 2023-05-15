2692988b76711bbad0f050cfc08b2fd05f40e560
Erick Ruh Cardozo <erickruhcardozo98@gmail.com>
2023-05-15 17:27:31 -0300

    Alterações no formulário de cadastro

--- pages/register.php
+++ pages/register.php
@@ -1,69 +1,26 @@
-<?php
-class nome{
-	private $nome;
-	private $email;
-	private $cor;
-	private $idade;
-
-	public function setnome($nome)
-	{
-		$this->nome=$nome;
-	}
-	public function getnome()
-	{
-		return $this->nome;
-
-}
-	public function setemail($email)
-	{
-		$this->email=$emailail;
-	}
-	public function getemail()
-	{
-		return $this->email;
-
-}	
-	
-	public function setidade($idade)
-	{
-		$this->idade=$idade;
-	}
-	public function getidade()
-	{
-		return $this->idade;
-
-}
-}
-?>
-<?php
-	$nome=new nome();
-	$nome->setnome($_POST['nome']);
-	$nome->setemail($_POST['email']);
-	$nome->setidade($_POST['idade']);
-?>
-
-
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR" data-bs-theme="dark">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous" defer></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+	<link rel="stylesheet" href="../css/index.css">
+    <title>Cadastro | Texugo Futebol Club</title>
 </head>
 <body>
-    <form action="register.php" method="POST">
-    <h1>Cadastro</h1>
-		<p>Nome:<input type="text" name="nome" size="30" maxlength="30" pattern="[0,9A-Za-z]{3,10}" title="Letras ou números sem caracteres especiais" required></p>
-		<p>Email:<input type="passaword" name="email" size="10" maxlength="10" required></p>
-		<p>Idade:<input type="date" name="idade" required></p>
-        <p><input type="nuber" name="botao" required></p>
-		<p><input type="submit" name="enviar" value="Cadastrar"></p>   
-</form> 
-<?php 
-    if (isset($_POST['botao'])){
-    $numero=
+	<div class="container">
+		<h1>Cadastro</h1>
+		<form action="register.php" method="POST">
+			<div>
+				<label class="form-label" for="name">Nome</label>
+				<input class="form-control" type="text" name="name" id="name">
+			</div>
 
-    }
+			<input class="btn btn-success w-100 mt-3" type="submit" value="Cadastrar">
+		</form>
+	</div>
 </body>
 </html>
